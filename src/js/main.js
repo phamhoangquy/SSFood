7034eeeace337271d0d98d3a5d7caa73276ab574
I'm Kai <53844429+voducmanh99@users.noreply.github.com>
2021-01-28 15:47:55 +0700

    update source

--- src/js/main.js
+++ src/js/main.js
@@ -1,316 +1,319 @@
 $(document).ready(function() {
-    setBackgroundElement();
-    mappingMenu();
-    toggleMobileMenu();
-    swiperInit();
-    productDetailSlide();
-    tabActive();
-    sideNav()
-    showBackToTop();
+	setBackgroundElement();
+	mappingMenu();
+	toggleMobileMenu();
+	swiperInit();
+	productDetailSlide();
+	tabActive();
+	sideNav()
+	showBackToTop();
 });
 
 function height(el) {
-    var height = 0;
-    $(el).each(function() {
-        var thisHeight = $(this).height();
-        if (thisHeight > height) {
-            height = thisHeight;
-        }
-        setTimeout(() => {
-            $(el).height(height)
-        }, 100)
-    })
+	var height = 0;
+	$(el).each(function() {
+		var thisHeight = $(this).height();
+		if (thisHeight > height) {
+			height = thisHeight;
+		}
+		setTimeout(() => {
+			$(el).height(height)
+		}, 100)
+	})
 }
 
 let header = {
-    scrollActive: function() {
-        let height = $('header').height()
-        if ($(window).scrollTop() > height) {
-            $('header').addClass('active')
-        } else {
-            $('header').removeClass('active')
-        }
-    },
+	scrollActive: function() {
+		let height = $('header').height()
+		if ($(window).scrollTop() > height) {
+			$('header').addClass('active')
+		} else {
+			$('header').removeClass('active')
+		}
+	},
 }
 
 function tabActive() {
-    $(".tab-navigation li a").on("click", function() {
-        $(this)
-            .parents(".tab-navigation")
-            .find("li")
-            .removeClass("active");
-        $(this)
-            .parents("li")
-            .addClass("active");
+	$(".tab-navigation li a").on("click", function() {
+		$(this)
+			.parents(".tab-navigation")
+			.find("li")
+			.removeClass("active");
+		$(this)
+			.parents("li")
+			.addClass("active");
 
-        var display = $(this).attr("data-type");
-        $(".tab-item").removeClass("active");
-        $("#" + display).addClass("active");
+		var display = $(this).attr("data-type");
+		$(".tab-item").removeClass("active");
+		$("#" + display).addClass("active");
 
-        let maxHeight = 400;
-        let contentTab = $(".tab-wrapper .tab-item.active");
-        // console.log(contentTab.height())
-        if (contentTab.height() < maxHeight) {
-            $(contentTab).find('.btn-view-more').hide()
-        }
-    });
+		let maxHeight = 400;
+		let contentTab = $(".tab-wrapper .tab-item.active");
+		// console.log(contentTab.height())
+		if (contentTab.height() < maxHeight) {
+			$(contentTab).find('.btn-view-more').hide()
+		}
+	});
 }
 
 function setBackgroundElement() {
-    $('[setBackground]').each(function() {
-        var background = $(this).attr('setBackground')
-        $(this).css({
-            "background-image": "url(" + background + ")",
-            "background-size": "cover",
-            "background-position": "center center"
-        });
-    });
-    $('[setBackgroundRepeat]').each(function() {
-        var background = $(this).attr('setBackgroundRepeat')
-        $(this).css({
-            "background-image": "url(" + background + ")",
-            "background-repeat": "repeat"
-        });
-    });
+	$('[setBackground]').each(function() {
+		var background = $(this).attr('setBackground')
+		$(this).css({
+			"background-image": "url(" + background + ")",
+			"background-size": "cover",
+			"background-position": "center center"
+		});
+	});
+	$('[setBackgroundRepeat]').each(function() {
+		var background = $(this).attr('setBackgroundRepeat')
+		$(this).css({
+			"background-image": "url(" + background + ")",
+			"background-repeat": "repeat"
+		});
+	});
 }
 // Slide product-detail
 function productDetailSlide() {
-    var galleryThumbs = new Swiper('.gallery-thumbs', {
-        spaceBetween: 20,
-        slidesPerView: 4,
-        freeMode: true,
-        watchSlidesVisibility: true,
-        watchSlidesProgress: true,
-        breakpoints: {
-            576: {
-                slidesPerView: 3,
-            },
-            1000: {
-                slidesPerView: 4,
-            }
-        }
-    });
-    var galleryTop = new Swiper('.gallery-top', {
-        spaceBetween: 20,
-        thumbs: {
-            swiper: galleryThumbs
-        },
-        navigation: {
-            nextEl: '.nav-next',
-            prevEl: '.nav-prev',
-        },
-    });
+	var galleryThumbs = new Swiper('.gallery-thumbs', {
+		spaceBetween: 20,
+		slidesPerView: 4,
+		freeMode: true,
+		watchSlidesVisibility: true,
+		watchSlidesProgress: true,
+		breakpoints: {
+			576: {
+				slidesPerView: 3,
+			},
+			1000: {
+				slidesPerView: 4,
+			}
+		}
+	});
+	var galleryTop = new Swiper('.gallery-top', {
+		spaceBetween: 20,
+		thumbs: {
+			swiper: galleryThumbs
+		},
+		navigation: {
+			nextEl: '.nav-next',
+			prevEl: '.nav-prev',
+		},
+	});
 }
 
 function toggleMobileMenu() {
-    $('.header_btn').click(function() {
-        $(this).toggleClass('click');
-        $('.menu_mobile').toggleClass('show');
-        $('.overlay').toggleClass('show');
-    });
-    $('.overlay').click(function() {
-        $(this).toggleClass('click');
-        $('.menu_mobile').toggleClass('show');
-        $('.overlay').toggleClass('show');
-    });
-    $('.search-icon').click(function() {
-        $(this).toggleClass("click");
-        $('.block_search').toggleClass("show");
-    });
+	$('.header_btn').click(function() {
+		$(this).toggleClass('click');
+		$('.menu_mobile').toggleClass('show');
+		$('.overlay').toggleClass('show');
+	});
+	$('.overlay').click(function() {
+		$(this).toggleClass('click');
+		$('.menu_mobile').toggleClass('show');
+		$('.overlay').toggleClass('show');
+	});
+	$('.search-icon').click(function() {
+		$(this).toggleClass("click");
+		$('.block_search').toggleClass("show");
+	});
 }
 
 
 function mappingMenu() {
-    $('header .menu-left .nav-menu').mapping({
-        mobileWrapper: 'header .menu_mobile .mobile-wrapper',
-        mobileMethod: 'appendTo',
-        desktopWrapper: 'header .menu-left',
-        desktopMethod: 'prependTo',
-        breakpoint: 1280
-    });
-    $('header .menu-right .nav-menu').mapping({
-        mobileWrapper: 'header .menu_mobile .mobile-wrapper',
-        mobileMethod: 'appendTo',
-        desktopWrapper: 'header .menu-right',
-        desktopMethod: 'prependTo',
-        breakpoint: 1280
-    });
-    $('header .menu-right .wapper-hotline .hotline').mapping({
-        mobileWrapper: 'header .menu_mobile .mobile-wrapper',
-        mobileMethod: 'appendTo',
-        desktopWrapper: 'header .menu-right .wapper-hotline',
-        desktopMethod: 'prependTo',
-        breakpoint: 1280
-    });
+	$('header .menu-left .nav-menu').mapping({
+		mobileWrapper: 'header .menu_mobile .mobile-wrapper',
+		mobileMethod: 'appendTo',
+		desktopWrapper: 'header .menu-left',
+		desktopMethod: 'prependTo',
+		breakpoint: 1280
+	});
+	$('header .menu-right .nav-menu').mapping({
+		mobileWrapper: 'header .menu_mobile .mobile-wrapper',
+		mobileMethod: 'appendTo',
+		desktopWrapper: 'header .menu-right',
+		desktopMethod: 'prependTo',
+		breakpoint: 1280
+	});
+	$('header .menu-right .wapper-hotline .hotline').mapping({
+		mobileWrapper: 'header .menu_mobile .mobile-wrapper',
+		mobileMethod: 'appendTo',
+		desktopWrapper: 'header .menu-right .wapper-hotline',
+		desktopMethod: 'prependTo',
+		breakpoint: 1280
+	});
 }
 
 function swiperInit() {
-    var homerSwiper = new Swiper(".home-banner .swiper-container", {
-        // Optional parameters
-        speed: 1205,
-        slidesPerView: 1,
-        slidesPerColumnFill: "row",
-        autoplay: {
-            delay: 3000
-        },
-        navigation: {
-            nextEl: '.home-banner .nav-arrow-next',
-            prevEl: '.home-banner .nav-arrow-prev',
-        },
-    });
-    var certificatesSwiper = new Swiper(".home_s-5 .swiper-container", {
-        // Optional parameters
-        speed: 1000,
-        spaceBetween: 20,
-        breakpointsInverse: true,
-        breakpoints: {
-            320: {
-                slidesPerView: 2,
-            },
-            400: {
-                slidesPerView: 2,
-            },
-            480: {
-                slidesPerView: 3,
-            },
-            768: {
-                slidesPerView: 4,
-            },
-            1025: {
-                slidesPerView: 5,
-            },
-            1440: {
-                slidesPerView: 5,
-            },
-        },
-    });
-    var aboutNewsSwiper = new Swiper(".about-2 .swiper-container", {
-        // Optional parameters
-        speed: 1000,
-        spaceBetween: 30,
-        breakpointsInverse: true,
-        navigation: {
-            nextEl: '.about-2 .nav-arrow-next',
-            prevEl: '.about-2 .nav-arrow-prev',
-        },
-        breakpoints: {
-            320: {
-                slidesPerView: 1,
-            },
-            400: {
-                slidesPerView: 1,
-            },
-            480: {
-                slidesPerView: 2,
-            },
-            768: {
-                slidesPerView: 3,
-            },
-            1025: {
-                slidesPerView: 4,
-            },
-            1440: {
-                slidesPerView: 4,
-            },
-        },
-    });
-    var aboutCertificatesSwiper = new Swiper(".about-4 .swiper-container", {
-        // Optional parameters
-        speed: 1000,
-        spaceBetween: 20,
-        breakpointsInverse: true,
-        navigation: {
-            nextEl: '.about-4 .nav-arrow-next',
-            prevEl: '.about-4 .nav-arrow-prev',
-        },
-        breakpoints: {
-            320: {
-                slidesPerView: 2,
-            },
-            400: {
-                slidesPerView: 2,
-            },
-            480: {
-                slidesPerView: 4,
-            },
-            768: {
-                slidesPerView: 5,
-            },
-            1025: {
-                slidesPerView: 6,
-            },
-            1440: {
-                slidesPerView: 7,
-            },
-        },
-    });
-    var aboutClientSwiper = new Swiper(".about-6 .swiper-container", {
-        // Optional parameters
-        speed: 1000,
-        spaceBetween: 20,
-        breakpointsInverse: true,
-        navigation: {
-            nextEl: '.about-6 .nav-arrow-next',
-            prevEl: '.about-6 .nav-arrow-prev',
-        },
-        breakpoints: {
-            320: {
-                slidesPerView: 2,
-            },
-            400: {
-                slidesPerView: 2,
-            },
-            480: {
-                slidesPerView: 4,
-            },
-            768: {
-                slidesPerView: 5,
-            },
-            1025: {
-                slidesPerView: 6,
-            },
-            1440: {
-                slidesPerView: 7,
-            },
-        },
-    });
+	var homerSwiper = new Swiper(".home-banner .swiper-container", {
+		// Optional parameters
+		speed: 1205,
+		slidesPerView: 1,
+		slidesPerColumnFill: "row",
+		autoplay: {
+			delay: 3000
+		},
+		navigation: {
+			nextEl: '.home-banner .nav-arrow-next',
+			prevEl: '.home-banner .nav-arrow-prev',
+		},
+	});
+	var certificatesSwiper = new Swiper(".home_s-5 .swiper-container", {
+		// Optional parameters
+		speed: 1000,
+		spaceBetween: 20,
+		breakpointsInverse: true,
+		breakpoints: {
+			320: {
+				slidesPerView: 2,
+			},
+			400: {
+				slidesPerView: 2,
+			},
+			480: {
+				slidesPerView: 3,
+			},
+			768: {
+				slidesPerView: 4,
+			},
+			1025: {
+				slidesPerView: 5,
+			},
+			1440: {
+				slidesPerView: 5,
+			},
+		},
+	});
+	var aboutNewsSwiper = new Swiper(".about-2 .swiper-container", {
+		// Optional parameters
+		speed: 1000,
+		spaceBetween: 30,
+		breakpointsInverse: true,
+		navigation: {
+			nextEl: '.about-2 .nav-arrow-next',
+			prevEl: '.about-2 .nav-arrow-prev',
+		},
+		breakpoints: {
+			320: {
+				slidesPerView: 1,
+			},
+			400: {
+				slidesPerView: 1,
+			},
+			480: {
+				slidesPerView: 2,
+			},
+			768: {
+				slidesPerView: 3,
+			},
+			1025: {
+				slidesPerView: 4,
+			},
+			1440: {
+				slidesPerView: 4,
+			},
+		},
+	});
+	var aboutCertificatesSwiper = new Swiper(".about-4 .swiper-container", {
+		// Optional parameters
+		speed: 1000,
+		spaceBetween: 20,
+		breakpointsInverse: true,
+		navigation: {
+			nextEl: '.about-4 .nav-arrow-next',
+			prevEl: '.about-4 .nav-arrow-prev',
+		},
+		breakpoints: {
+			320: {
+				slidesPerView: 2,
+			},
+			400: {
+				slidesPerView: 2,
+			},
+			480: {
+				slidesPerView: 4,
+			},
+			768: {
+				slidesPerView: 5,
+			},
+			1025: {
+				slidesPerView: 6,
+			},
+			1440: {
+				slidesPerView: 7,
+			},
+		},
+	});
+	var aboutClientSwiper = new Swiper(".about-6 .swiper-container", {
+		// Optional parameters
+		speed: 1000,
+		spaceBetween: 20,
+		breakpointsInverse: true,
+		navigation: {
+			nextEl: '.about-6 .nav-arrow-next',
+			prevEl: '.about-6 .nav-arrow-prev',
+		},
+		breakpoints: {
+			320: {
+				slidesPerView: 2,
+			},
+			400: {
+				slidesPerView: 2,
+			},
+			480: {
+				slidesPerView: 4,
+			},
+			768: {
+				slidesPerView: 5,
+			},
+			1025: {
+				slidesPerView: 6,
+			},
+			1440: {
+				slidesPerView: 7,
+			},
+		},
+	});
 }
 
 // toggle-product-list
 function sideNav() {
-    let trigger = $('.item-product-list .content ul .title-pro-zone span');
-    let close = $('.item-product-list .content .sub-product');
-    let parent = $('.item-product-list .content ul li');
-    trigger.on('click', function(e) {
-        e.preventDefault();
-        if (!$(this).parent().parent().hasClass("pro")) {
-            close.slideUp();
-            $(this).parent().next().slideToggle();
-            parent.removeClass('pro')
-            $(this).parents('li').addClass('pro')
-        } else {
-            $(this).parent().next().slideToggle();
-            parent.removeClass('pro')
-        }
-    })
+	let btn = $('.product-list .item-product-list .content ul li .title-pro-zone span');
+	let subMenuProduct = $('.product-list .item-product-list .content .sub-product');
+	if($('.product-list .item-product-list .content ul li.has-dropdown').hasClass('active')){
+		$('.product-list .item-product-list .content ul li.has-dropdown.active .sub-product').slideDown();
+	}
+	btn.on('click', function(){
+		
+		if($(this).parents('.has-dropdown').hasClass('active')){
+			$('.product-list .item-product-list .content ul li.has-dropdown').removeClass('active')
+			// console.log(1);
+			subMenuProduct.slideUp();
+			$(this).parents('.has-dropdown').removeClass('active').find('.sub-product').slideUp();
+		}
+		else{
+			// console.log(2);
+			subMenuProduct.slideUp();
+			$(this).parents('.has-dropdown').addClass('active').find('.sub-product').slideDown();
+		}
+	})
 }
-
 function showBackToTop() {
-    $(window).scroll(function() {
-        if ($(this).scrollTop() > 0) {
-            $('#back-to-top').addClass('active');
-        } else {
-            $('#back-to-top').removeClass('active');
-        }
-    });
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > 0) {
+			$('#back-to-top').addClass('active');
+		} else {
+			$('#back-to-top').removeClass('active');
+		}
+	});
 
-    $("#back-to-top").on("click", function(e) {
-        e.preventDefault();
-        $("html,body").animate({
-            scrollTop: 0
-        })
-    })
+	$("#back-to-top").on("click", function(e) {
+		e.preventDefault();
+		$("html,body").animate({
+			scrollTop: 0
+		})
+	})
 }
 $(document).on('scroll', function() {
-    header.scrollActive()
+	header.scrollActive()
 });
--- xslt/zone/LeftMenu.xslt
+++ xslt/zone/LeftMenu.xslt
@@ -1,70 +1,82 @@
 <?xml version="1.0" encoding="utf-8"?>
 <xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
   xmlns:msxsl="urn:schemas-microsoft-com:xslt" exclude-result-prefixes="msxsl">
-  <xsl:output method="html" indent="yes" />
+	<xsl:output method="html" indent="yes" />
+	<xsl:template match="/ZoneList">
+		<xsl:if test="count(Zone) > 0">
+			<div class="item-product-list">
+				<div class="sub-title-product">
+					<h2>
+						<xsl:value-of disable-output-escaping="yes" select="ModuleTitle"></xsl:value-of>
+					</h2>
+				</div>
+				<div class="content">
+					<ul>
+						<xsl:apply-templates select="Zone"></xsl:apply-templates>
+					</ul>
+				</div>
+			</div>
+		</xsl:if>
+	</xsl:template>
+	<xsl:template match="Zone">
+		<li>
+			<xsl:if test="IsActive='true'">
+				<xsl:attribute name="class">
+					<xsl:text>active</xsl:text>
+				</xsl:attribute>
+			</xsl:if>
 
-  <xsl:template match="/ZoneList">
-    <xsl:if test="count(Zone) > 0">
-      <div class="item-product-list">
-        <div class="sub-title-product">
-          <h2>
-            <xsl:value-of disable-output-escaping="yes" select="ModuleTitle"></xsl:value-of>
-          </h2>
-        </div>
-        <div class="content">
-          <ul>
-            <xsl:apply-templates select="Zone"></xsl:apply-templates>
-          </ul>
-        </div>
-      </div>
-    </xsl:if>
-  </xsl:template>
+			<xsl:if test="count(Zone) > 0">
+				<xsl:attribute name="class">
+					<xsl:text>has-dropdown</xsl:text>
+				</xsl:attribute>
+			</xsl:if>
 
-  <xsl:template match="Zone">
-    <li>
-      <xsl:if test="IsActive='true'">
-        <xsl:attribute name="class">
-          <xsl:text>pro</xsl:text>
-        </xsl:attribute>
-      </xsl:if>
-      <div class="title-pro-zone">
-        <a>
-          <xsl:attribute name="href">
-            <xsl:value-of select="Url"></xsl:value-of>
-          </xsl:attribute>
-          <xsl:attribute name="target">
-            <xsl:value-of select="Target"></xsl:value-of>
-          </xsl:attribute>
-          <xsl:text> </xsl:text>
-          <xsl:value-of select="Title" disable-output-escaping="yes"></xsl:value-of>
-        </a>
-        <span class="ri-arrow-down-s-line"></span>
-      </div>
-      <xsl:if test="count(Zone) > 0">
-        <ul class="sub-product">
-          <xsl:apply-templates select="Zone" mode="Child"></xsl:apply-templates>
-        </ul>
-      </xsl:if>
-    </li>
-  </xsl:template>
+			<xsl:if test="IsActive='true' and count(Zone) > 0">
+				<xsl:attribute name="class">
+					<xsl:text>has-dropdown active</xsl:text>
+				</xsl:attribute>
+			</xsl:if>
 
-  <xsl:template match="Zone" mode="Child">
-    <li class="pro">
-      <xsl:if test="IsActive='true'">
-        <xsl:attribute name="class">
-          <xsl:text>pro active</xsl:text>
-        </xsl:attribute>
-      </xsl:if>
-      <a>
-        <xsl:attribute name="href">
-          <xsl:value-of select="Url"></xsl:value-of>
-        </xsl:attribute>
-        <xsl:attribute name="target">
-          <xsl:value-of select="Target"></xsl:value-of>
-        </xsl:attribute>
-        <xsl:value-of select="Title" disable-output-escaping="yes"></xsl:value-of>
-      </a>
-    </li>
-  </xsl:template>
-
+			<div class="title-pro-zone">
+				<a>
+					<xsl:attribute name="href">
+						<xsl:value-of select="Url"></xsl:value-of>
+					</xsl:attribute>
+					<xsl:attribute name="target">
+						<xsl:value-of select="Target"></xsl:value-of>
+					</xsl:attribute>
+					<xsl:text> </xsl:text>
+					<xsl:value-of select="Title" disable-output-escaping="yes"></xsl:value-of>
+				</a>
+				<xsl:if test="count(Zone) > 0">
+					<span class="ri-arrow-down-s-line"></span>
+				</xsl:if>
+			</div>
+			<xsl:if test="count(Zone) > 0">
+				<ul class="sub-product">
+					<xsl:apply-templates select="Zone" mode="Child"></xsl:apply-templates>
+				</ul>
+			</xsl:if>
+		</li>
+	</xsl:template>
+	<xsl:template match="Zone" mode="Child">
+		<li class="pro">
+			<xsl:if test="IsActive='true'">
+				<xsl:attribute name="class">
+					<xsl:text>pro active</xsl:text>
+				</xsl:attribute>
+			</xsl:if>
+			<a>
+				<xsl:attribute name="href">
+					<xsl:value-of select="Url"></xsl:value-of>
+				</xsl:attribute>
+				<xsl:attribute name="target">
+					<xsl:value-of select="Target"></xsl:value-of>
+				</xsl:attribute>
+				<xsl:value-of select="Title" disable-output-escaping="yes"></xsl:value-of>
+			</a>
+		</li>
+	</xsl:template>
 </xsl:stylesheet>
+
